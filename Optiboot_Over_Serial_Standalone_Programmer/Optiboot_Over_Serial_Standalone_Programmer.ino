02f4d60ad9d0b14140039ca527a7bc5ca6c5a68b
Russell Cottier <info@russellcottier.com>
2021-01-04 14:11:01 +0000

    Comments added for Sketch explanation

--- Optiboot_Over_Serial_Standalone_Programmer/Optiboot_Over_Serial_Standalone_Programmer.ino
+++ Optiboot_Over_Serial_Standalone_Programmer/Optiboot_Over_Serial_Standalone_Programmer.ino
@@ -1,3 +1,8 @@
+/* This sketch will allow programming of an Atmega 328 or similar that has optiboot installed, over serial UART 
+ *  Currently set up for Teensy 3.6 to proigram a 328AU over Serial prot 2 (Teensy 3.6 pins 9 and 10) connected 
+ *  to the Serial UART on the 32* directly.
+ */
+
 #define syncTimeout       1000
 #define responseTimeout   1000
 
